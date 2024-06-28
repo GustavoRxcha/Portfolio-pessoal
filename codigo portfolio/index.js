da6cbfd4a60d3532180625d0b51e10d97f324549
Gustavo Rocha <gustavorxcha@gmail.com>
2024-06-27 21:53:19 -0300

    API GITHUB

--- codigo portfolio/index.js
+++ codigo portfolio/index.js
@@ -65,3 +65,33 @@ $(document).ready(function(){
       });
   });
   
+  const repositories = document.querySelector('.content-main');
+
+function getApiGitHub() {
+    fetch('https://api.github.com/users/GustavoRxcha/repos')
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(repo => {
+                let project = document.createElement('div');
+                project.className = 'project';
+
+                project.innerHTML = `
+                    <div>
+                        <h4 class="title">${repo.name}</h4>
+                        <span class="date-create">${new Date(repo.created_at).toLocaleDateString()}</span>
+                    </div>
+                    <div>
+                        <a href="${repo.html_url}" target="_blank">${repo.html_url}</a>
+                        <span class="language">
+                            <span class="circle">●</span>${repo.language || 'N/A'}
+                        </span>
+                    </div>
+                `;
+
+                repositories.appendChild(project);
+            });
+        })
+        .catch(error => console.error('Erro:', error));
+}
+
+getApiGitHub();
